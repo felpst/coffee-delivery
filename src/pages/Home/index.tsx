f442a5722eb20bff4d5f2afbf8b194e275de7d1c
felpst <felipe.t.v.d@gmail.com>
2022-09-19 10:15:13 -0300

    Creating the coffee card

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,33 @@
 import coffeeBanner from '../../assets/coffeeBanner.svg'
+import coffee from '../../assets/coffee.svg'
 import {
+  BuyDashboardContainer,
+  CoffeeListContainer,
+  CoffeeListItem,
+  CoffeeListItemsContainer,
+  CounterButton,
   HomeContainer,
   IconBrown,
+  IconGray,
+  IconPurple,
   IconYellow,
   IntroContainer,
   IntroItemContainer,
   IntroText,
+  Item,
+  Price,
+  Tag,
 } from './styles'
 
-import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
+import {
+  Coffee,
+  Minus,
+  Package,
+  Plus,
+  ShoppingCart,
+  ShoppingCartSimple,
+  Timer,
+} from 'phosphor-react'
 
 export function Home() {
   return (
@@ -21,30 +40,65 @@ export function Home() {
             hora
           </p>
           <IntroItemContainer>
-            <div>
+            <Item>
               <IconBrown>
                 <ShoppingCart size={16} weight="fill" color="#fff" />
               </IconBrown>
-              <span>Compre simples e segura</span>
-            </div>
-            <div>
-              <IconYellow>
+              <span>Compra simples e segura</span>
+            </Item>
+            <Item>
+              <IconGray>
                 <Package size={16} weight="fill" color="#fff" />
-              </IconYellow>
+              </IconGray>
               <span>Embalagem mantém o café intacto</span>
-            </div>
-            <div>
-              <Timer size={16} weight="fill" color="#fff" />
+            </Item>
+            <Item>
+              <IconYellow>
+                <Timer size={16} weight="fill" color="#fff" />
+              </IconYellow>
               <span>Entrega rápida e rastreada</span>
-            </div>
-            <div>
-              <Coffee size={16} weight="fill" color="#fff" />
+            </Item>
+            <Item>
+              <IconPurple>
+                <Coffee size={16} weight="fill" color="#fff" />
+              </IconPurple>
               <span>O café chega fresquinho até você</span>
-            </div>
+            </Item>
           </IntroItemContainer>
         </IntroText>
         <img src={coffeeBanner} alt="" />
       </IntroContainer>
+      <CoffeeListContainer>
+        <h1>Nossos cafés</h1>
+        <CoffeeListItemsContainer>
+          <CoffeeListItem>
+            <img src={coffee} alt="" />
+            <Tag>Tradicional</Tag>
+            <span>Nome do café</span>
+            <p>O tradicional café feito com água quente e grãos moídos</p>
+            <BuyDashboardContainer>
+              <Price>
+                <p>R$</p>
+                <span>0,00</span>
+              </Price>
+              <div>
+                <CounterButton>
+                  <button>
+                    <Minus size={14} weight="bold" color="#8047F8" />
+                  </button>
+                  <span>0</span>
+                  <button>
+                    <Plus size={14} weight="bold" color="#8047F8" />
+                  </button>
+                </CounterButton>
+                <button>
+                  <ShoppingCartSimple size={16} weight="fill" color="#ffffff" />
+                </button>
+              </div>
+            </BuyDashboardContainer>
+          </CoffeeListItem>
+        </CoffeeListItemsContainer>
+      </CoffeeListContainer>
     </HomeContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -9,6 +9,7 @@ export const HomeContainer = styled.main`
 export const IntroContainer = styled.div`
   display: flex;
   flex-direction: row;
+  padding: 5.875rem 0 6.75rem 0;
 `
 
 export const IntroText = styled.div`
@@ -80,7 +81,174 @@ export const IconGray = styled.div`
 export const IconPurple = styled.div`
   width: 2rem;
   height: 2rem;
-  background: ${(props) => props.theme['purple-light']};
+  background: ${(props) => props.theme.purple};
   border-radius: 100%;
   padding: 0.5rem;
+`
+export const Item = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.75rem;
+`
+
+export const CoffeeListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  h1 {
+    font-family: 'Baloo 2';
+    font-size: 2rem;
+    line-height: 130%;
+    font-weight: 800;
+    color: ${(props) => props.theme['base-subtitle']};
+    padding-bottom: 2.125rem;
+  }
+`
+
+export const CoffeeListItemsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+
+  padding-bottom: 9.8125rem;
+`
+
+export const CoffeeListItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  background: ${(props) => props.theme['base-card']};
+
+  width: 16rem;
+  height: 19.375rem;
+  border-radius: 6px 36px 6px 36px;
+
+  img {
+    margin-top: calc(0px - 1.5rem);
+  }
+
+  span {
+    font-family: 'Baloo 2';
+    font-size: 1.25rem;
+    font-weight: 700;
+    line-height: 130%;
+
+    margin-top: 1rem;
+  }
+
+  p {
+    font-family: 'Roboto';
+    font-size: 0.875rem;
+    font-weight: 400;
+    line-height: 130%;
+    text-align: center;
+
+    color: ${(props) => props.theme['base-label']};
+    width: 216px;
+
+    margin-top: 0.5rem;
+  }
+`
+
+export const Tag = styled.div`
+  background: ${(props) => props.theme['yellow-light']};
+  color: ${(props) => props.theme['yellow-dark']};
+
+  padding: 0.25rem 0.5rem;
+  border-radius: 999px;
+
+  margin-top: 0.75rem;
+
+  font-family: 'Roboto';
+  font-size: 0.625rem;
+  font-weight: 700;
+  line-height: 130%;
+`
+
+export const BuyDashboardContainer = styled.div`
+  width: 13rem;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: 2rem;
+
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 2.375rem;
+    padding: 0px 0.65rem;
+
+    background: ${(props) => props.theme['purple-dark']};
+
+    border: none;
+    border-radius: 6px;
+  }
+
+  div {
+    display: flex;
+    flex-direction: row;
+    gap: 0.5rem;
+  }
+`
+
+export const Price = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: baseline;
+
+  gap: 0.25rem;
+
+  span {
+    margin: 0px;
+    font-family: 'Baloo 2';
+    font-size: 1.5rem;
+    font-weight: 800;
+    line-height: 130%;
+  }
+
+  p {
+    width: min-content;
+    color: ${(props) => props.theme['base-text']};
+    margin: 0px;
+    font-size: 0.875rem;
+  }
+`
+
+export const CounterButton = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  background: ${(props) => props.theme['base-button']};
+
+  height: 2.375rem;
+  border-radius: 6px;
+
+  span {
+    margin: 0px;
+    font-family: 'Roboto';
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 100%;
+    color: ${(props) => props.theme['base-title']};
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    border: none;
+    background: none;
+    height: 0.875rem;
+    padding: 0.35rem;
+  }
 `
