5833df7421a92950480f614f1e3d56bd6615659c
Felipe Veloso <felipe@Felipes-MacBook-Pro.local>
2022-10-20 09:57:53 -0300

    implementing zod

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -2,9 +2,23 @@ import { useForm } from 'react-hook-form'
 import { OrderInfo } from './components/OrderInfo'
 import { Selection } from './components/Selection'
 import { Container } from './styles'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+const newFormValidationSchema = z.object({
+  cep: z.string().min(8).max(8),
+  rua: z.string(),
+  numero: z.number(),
+  complemento: z.string(),
+  bairro: z.string(),
+  cidade: z.string(),
+  uf: z.string().min(2).max(2),
+})
 
 export function Checkout() {
-  const { register, handleSubmit } = useForm()
+  const { register, handleSubmit } = useForm({
+    resolver: zodResolver(newFormValidationSchema),
+  })
 
   return (
     <Container>
